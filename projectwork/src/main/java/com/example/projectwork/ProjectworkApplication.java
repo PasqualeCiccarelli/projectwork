00a9e0c91cc007232f628eb69d2ca3a1dae8158b
Pasquale Ciccarelli <pasqualeciccarelli20@gmail.com>
2025-01-10 10:14:04 +0100

    startingproject

---
+++ projectwork/src/main/java/com/example/projectwork/ProjectworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectworkApplication.class, args);
+	}
+
+}
