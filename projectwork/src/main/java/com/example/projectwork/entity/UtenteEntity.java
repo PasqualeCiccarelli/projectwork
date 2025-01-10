ecf0b7676ef712ce839943e8983bdf5fd545cec8
Pasquale Ciccarelli <pasqualeciccarelli20@gmail.com>
2025-01-10 16:40:21 +0100

    entity

--- projectwork/src/main/java/com/example/projectwork/entity/UtenteEntity.java
+++ projectwork/src/main/java/com/example/projectwork/entity/UtenteEntity.java
@@ -2,4 +2,10 @@ package com.example.projectwork.entity;
 
 public class UtenteEntity {
 
+	private long id;
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private Ruolo ruolo;
 }
