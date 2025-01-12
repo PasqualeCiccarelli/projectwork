fa2f4742c6fbac00011813a0e6045ddf0b6ac38c
massimo.viola <183734774+massimoviola@users.noreply.github.com>
2025-01-12 18:08:22 +0100

    loghi,novità,gadgets

--- projectwork/src/main/resources/static/switch-style.js
+++ projectwork/src/main/resources/static/switch-style.js
@@ -5,25 +5,32 @@ const btnPokemon = document.querySelector("#btn-pokemon");
 const btnYugi = document.querySelector("#btn-yugi");
 
 const btnMagic = document.querySelector("#btn-magic");
+const novita = document.getElementById("Novita");
+const gadgets = document.getElementById("Gadgets");
 
 btnPokemon.addEventListener("click", () => {
-    background.classList.remove("background-base");
-    background.classList.remove("background-yugi");
-    background.classList.remove("background-magic");
-    background.classList.add("background-pokemon");
+  background.classList.remove("background-base");
+  background.classList.remove("background-yugi");
+  background.classList.remove("background-magic");
+  background.classList.add("background-pokemon");
+  novita.src = "../static/img/immagini-font/Novita.png";
+  gadgets.src = "../static/img/immagini-font/gadgets.png";
 });
 
 btnYugi.addEventListener("click", () => {
-    background.classList.remove("background-base");
-    background.classList.remove("background-pokemon");
-    background.classList.remove("background-magic");
-    background.classList.add("background-yugi");
+  background.classList.remove("background-base");
+  background.classList.remove("background-pokemon");
+  background.classList.remove("background-magic");
+  background.classList.add("background-yugi");
+  novita.src = "../static/img/immagini-font/yugi-novita.png";
+  gadgets.src = "../static/img/immagini-font/yugi-gadgets.png";
 });
 
 btnMagic.addEventListener("click", () => {
-    background.classList.remove("background-base");
-    background.classList.remove("background-pokemon");
-    background.classList.remove("background-yugi");
-    background.classList.add("background-magic");
+  background.classList.remove("background-base");
+  background.classList.remove("background-pokemon");
+  background.classList.remove("background-yugi");
+  background.classList.add("background-magic");
+  novita.src = "../static/img/immagini-font/magic-novità.png";
+  gadgets.src = "../static/img/immagini-font/magic-gadgets.png";
 });
-
