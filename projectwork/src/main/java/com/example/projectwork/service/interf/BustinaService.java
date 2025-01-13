f21ab5b0d26b2181504df9e5d98aa064a8994670
Giuseppe M. <170727995+JoJoR3D@users.noreply.github.com>
2025-01-13 14:43:22 +0100

    Get box e bustine Admin

--- projectwork/src/main/java/com/example/projectwork/service/interf/BustinaService.java
+++ projectwork/src/main/java/com/example/projectwork/service/interf/BustinaService.java
@@ -2,9 +2,12 @@ package com.example.projectwork.service.interf;
 
 import java.util.List;
 
+import com.example.projectwork.dto.AccessoriDto;
 import com.example.projectwork.dto.BustinaDto;
 import com.example.projectwork.dto.BustinaRequest;
 import com.example.projectwork.entity.BustinaEntity;
+import com.example.projectwork.entity.entityenum.Brand;
+import com.example.projectwork.entity.entityenum.Categoria;
 
 public interface BustinaService {
 	
@@ -12,5 +15,6 @@ public interface BustinaService {
 	public List<BustinaDto> getBustineByBrandPokemon();
 	public List<BustinaDto> getBustineByBrandMagic();
 	public List<BustinaDto> getBustineByBrandYugiho();
+	public List<BustinaDto> getCardByAccessori(Brand brand, Categoria categoria);
 
 }
