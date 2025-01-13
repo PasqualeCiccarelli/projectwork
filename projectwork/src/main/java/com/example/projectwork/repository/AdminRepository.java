0e255775b8f584a6613c60203406aa60864cb63f
Giuseppe M. <170727995+JoJoR3D@users.noreply.github.com>
2025-01-13 11:45:45 +0100

    commit

--- projectwork/src/main/java/com/example/projectwork/repository/AdminRepository.java
+++ projectwork/src/main/java/com/example/projectwork/repository/AdminRepository.java
@@ -9,4 +9,5 @@ import com.example.projectwork.entity.AdminEntity;
 public interface AdminRepository extends JpaRepository<AdminEntity, Long>{
 	
 	Optional<AdminEntity> findByEmail(String Email);
+	
 }
