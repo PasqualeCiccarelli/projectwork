82e23e257273afd07b8b9c32aea31446c7a56f40
Pasquale Ciccarelli <pasqualeciccarelli20@gmail.com>
2025-01-16 15:32:12 +0100

    utente

--- projectwork/src/main/java/com/example/projectwork/service/interf/UtenteService.java
+++ projectwork/src/main/java/com/example/projectwork/service/interf/UtenteService.java
@@ -1,5 +1,10 @@
 package com.example.projectwork.service.interf;
 
+import com.example.projectwork.entity.UtenteEntity;
+
 public interface UtenteService {
+	
+	public String aggiornaImmagineProfilo(String email, String nuovaImmagine);
+	public UtenteEntity getUtenteByEmail(String email);
 
 }
